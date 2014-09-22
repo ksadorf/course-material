0df4984a8dcc331d08c7b513a20aaa426ed7d670
Noe Gaumont <noe.gaumont@gmail.com>
2014-09-22 15:51:07 +0200

    PEP

--- exercices/200/solution.py
+++ exercices/200/solution.py
@@ -3,7 +3,7 @@ def is_prime(n):
         return False
     if n == 2:
         return True
-    for i in range(3,int(n**0.5)+1,2):
+    for i in range(3, int(n**0.5)+1, 2):
         if n % i == 0:
             return False
 
