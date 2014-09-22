af51244be59c1cf186e930a50847eb6403122486
Noe Gaumont <noe.gaumont@gmail.com>
2014-09-22 12:00:05 +0200

    060

--- exercices/060/solution.py
+++ exercices/060/solution.py
@@ -1,6 +1,6 @@
-from itertools import combinations
+from itertools import product
 import string
 allTheLetters = string.ascii_lowercase
 
-for u,v in combinations(allTheLetters,2):
-	print(u,v)
+for u,v in product(allTheLetters,allTheLetters):
+	print(u+v)
