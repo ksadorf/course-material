de43a6bad014598106a6e8c98ea486fd95217d1b
Noe Gaumont <noe.gaumont@gmail.com>
2014-09-22 14:06:48 +0200

    FIXED PEP

--- exercices/002/solution.py
+++ exercices/002/solution.py
@@ -4,9 +4,9 @@ import string
 
 allTheLetters = string.ascii_lowercase
 for i in allTheLetters[::-1]:
-	if i in 'aeiou':
-		print(i.upper(),end="")
-	else:
-		print(i,end="")
+    if i in 'aeiou':
+        print(i.upper(), end="")
+    else:
+        print(i, end="")
 
 print()
