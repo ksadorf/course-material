7c8f2322d0d4db8bae87bc0f3ce9f6599aa97f4c
Noe Gaumont <noe.gaumont@gmail.com>
2014-09-23 15:17:37 +0200

    450

--- exercices/450/solution.py
+++ exercices/450/solution.py
@@ -1,9 +1,15 @@
 from string import ascii_lowercase, ascii_uppercase
 
 
+backward = 'backward'
+forward = 'forward'
+
+
 def caesar_cypher(s, k, mod):
     base = ord('a')
     for i in s:
+        if i in ascii_uppercase:
+            base = ord('A')
         if i in ascii_uppercase or i in ascii_lowercase:
             if mod == "forward":
                 dec = (ord(i) + k - base) % 26 + base
