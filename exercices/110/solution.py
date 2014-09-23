bfc07c71769c379d2146691fa6c639a93ae57799
Noe Gaumont <noe.gaumont@gmail.com>
2014-09-23 09:25:08 +0200

    Fixe usage

--- exercices/110/solution.py
+++ exercices/110/solution.py
@@ -2,13 +2,14 @@ import sys
 
 op = '+-*/%'
 if len(sys.argv) != 4 or sys.argv[2] not in op:
-    print('usage: ./calc.py a_number (an_operator +-*/%) a_number')
+    print(
+        'usage: python3 ./solution.py a_number (an_operator +-*/%^) a_number')
     exit(0)
 n1 = int(sys.argv[1])
 n2 = int(sys.argv[3])
 o = sys.argv[2]
 if o == '+':
-        print(n1 + n2)
+    print(n1 + n2)
 elif o == '-':
     print(n1 - n2)
 elif o == '*':
